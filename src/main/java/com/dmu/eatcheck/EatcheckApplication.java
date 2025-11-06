687347e070f5209eb305c45a1491a3b943c248ca
minjinhong <wlsghddl3358@naver.com>
2025-11-06 10:38:39 +0900

    start project

---
+++ src/main/java/com/dmu/eatcheck/EatcheckApplication.java
@@ -0,0 +1,13 @@
+package com.dmu.eatcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatcheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmu/eatcheck/EatcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmu.eatcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatcheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
