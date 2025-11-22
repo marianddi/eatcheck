ec038aeb63a28a49cef3ef5f9529ebd703d827c9
wls313 <71283522+wls313@users.noreply.github.com>
2025-11-22 21:06:20 +0900

    Fix : 테스트를 위한 jwt 토큰 부분 제거

--- src/main/java/com/dmu/eatcheck/features/auth/signUp/domain/dto/SignUpResponseDto.java
+++ src/main/java/com/dmu/eatcheck/features/auth/signUp/domain/dto/SignUpResponseDto.java
@@ -8,14 +8,12 @@ import lombok.Getter;
 public class SignUpResponseDto {
     private final String message;
     private final String userId;
-    private final String accessToken;
     private final Integer id;
 
-    public static SignUpResponseDto success(String userId, String token, Integer id) {
+    public static SignUpResponseDto success(String userId, Integer id) {
         return SignUpResponseDto.builder()
                 .message("회원가입이 성공적으로 완료되었습니다. 프로필 기입이 필요합니다.")
                 .userId(userId)
-                .accessToken(token)
                 .id(id)
                 .build();
     }
