0b354f0b5f0041d36a5cb5031a13d43093c43e34
wls313 <71283522+wls313@users.noreply.github.com>
2025-11-24 19:06:45 +0900

    Feature : 회원가입후 프로필 입력을하면 알려주신 계산식에 맞춰서 영양성분 저장하게 했습니다.

--- src/main/java/com/dmu/eatcheck/features/auth/signUp/domain/dto/ProfileResponseDto.java
+++ src/main/java/com/dmu/eatcheck/features/auth/signUp/domain/dto/ProfileResponseDto.java
@@ -10,4 +10,9 @@ public class ProfileResponseDto {
     private final Integer profileId;
     private final String userId;
     private final Integer bmr;
+    private final Integer tdee;
+    private final Integer recommendedCalorie;
+    private final Integer recommendedCarb;
+    private final Integer recommendedProtein;
+    private final Integer recommendedFat;
 }
