70f0103c8686de37b2b3d34536874615e97d774e
marianddi <hanna4331@naver.com>
2025-11-27 12:40:33 +0900

    dto
    dto만들기 전 커밋

---
+++ backend/src/main/java/com/dmu/eatcheck/dto/response/MyPageInfo.java
@@ -0,0 +1,12 @@
+package com.dmu.eatcheck.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class MyPageInfo {
+    //private
+}
