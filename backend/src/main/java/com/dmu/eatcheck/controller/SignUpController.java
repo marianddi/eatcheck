c35bd24d1daea42a0f4f05109cd9a0064461b671
wls313 <71283522+wls313@users.noreply.github.com>
2025-11-30 05:26:50 +0900

    FIX : 회원가입시 아이디 체크 기능 추가했습니다.

--- backend/src/main/java/com/dmu/eatcheck/controller/SignUpController.java
+++ backend/src/main/java/com/dmu/eatcheck/controller/SignUpController.java
@@ -11,10 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -51,6 +48,28 @@ public class SignUpController {
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("서버 오류로 인해 회원가입에 실패했습니다.");
+        }
+    }
+
+    @GetMapping("/checkId")
+    @ResponseBody
+    public ResponseEntity<String> checkUserId(@RequestParam String userId) {
+        try {
+            signUpService.validateDuplicateUser(userId, null, null);
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body("사용 가능한 아이디입니다.");
+
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(e.getMessage());
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("서버 오류로 인해 중복 체크에 실패했습니다.");
         }
     }
 }
