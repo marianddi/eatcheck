14f686bfeaaae11a585987e3eaf542b7b2beb237
marianddi <hanna4331@naver.com>
2025-12-10 02:55:21 +0900

    로그인 누락된거 추가

--- backend/src/main/java/com/dmu/eatcheck/service/UserService.java
+++ backend/src/main/java/com/dmu/eatcheck/service/UserService.java
@@ -1,6 +1,8 @@
 package com.dmu.eatcheck.service;
 //Service : 비즈니스 로직(db값 로직)
 
+import com.dmu.eatcheck.dto.response.GenericResponse;
+import com.dmu.eatcheck.dto.response.LoginResponse;
 import com.dmu.eatcheck.entity.User;
 import com.dmu.eatcheck.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -28,5 +30,13 @@ public class UserService {
         // 비밀번호 비교
         return rawPassword.equals(user.getPassword());
         //return passwordEncoder.matches(rawPassword, user.getPassword());
+    }
+
+    //로그인 시 프론트 전달할 것(userPk, nickname)
+    public LoginResponse loginResponse(String userId){
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(()->new RuntimeException("유저정보 없음."));
+        LoginResponse loginData = new LoginResponse(user.getId(), user.getNickname());
+        return loginData;
     }
 }
