ac3aa347a0e035b8dc21d1b5ef1e04e48a2c0424
wls313 <71283522+wls313@users.noreply.github.com>
2025-12-09 17:28:58 +0900

    feature : 몸무게 저장 추가

--- backend/src/main/java/com/dmu/eatcheck/entity/User.java
+++ backend/src/main/java/com/dmu/eatcheck/entity/User.java
@@ -42,4 +42,7 @@ public class User {
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private UserProfile userProfile;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WeightRecord> weightRecords;
 }
